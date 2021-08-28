4b1da033ef71a185a4c57d61c02c5b2705bfd62a
Tomasz Mankin <tomasz@tentixo.com>
2021-08-28 19:38:46 +0200

    Training complete

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -14,6 +14,7 @@ func WriteToConsole(next http.Handler) http.Handler {
 	})
 }
 
+//NoSurf adds CSFR protection to all POST requests
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
@@ -25,4 +26,9 @@ func NoSurf(next http.Handler) http.Handler {
 	})
 
 	return csrfHandler
+}
+
+//SessionLoad loads and saves the session on every request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
